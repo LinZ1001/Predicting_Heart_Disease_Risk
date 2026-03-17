372084840fac5648eae659f2bb434fbd614de885
Linda Zoungrana <lindainesz10@gmail.com>
2026-03-16 21:29:42 -0400

    Implement data cleaning and export to CSV
    Added data cleaning steps including missing value check and categorical variable conversion.

--- Scripts/02_clean_data.R
+++ Scripts/02_clean_data.R
@@ -1,1 +1,10 @@
+# Check missing values
+colSums(is.na(heart))
 
+# Convert categorical variables
+heart$sex <- factor(heart$sex)
+heart$cp <- factor(heart$cp)
+heart$target <- heart$num
+heart$target <- factor(heart$target)
+
+write.csv(heart, "/Users/---/Downloads/heart_clean.csv", row.names = FALSE)
