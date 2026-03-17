73b5c2e2e9b3b6839267a67b1dfb2297e09d169c
Linda Zoungrana <lindainesz10@gmail.com>
2026-03-16 21:31:41 -0400

    Add Random Forest model implementation

--- Scripts/05_random_forest.R
+++ Scripts/05_random_forest.R
@@ -1,1 +1,10 @@
+#Random Forest Model
+install.packages("randomForest")
+library(randomForest)
 
+heart$target <- as.factor(heart$target)
+heart <- na.omit(heart)
+rf_model <- randomForest(target ~ ., data = heart, importance = TRUE)
+
+print(rf_model)
+importance(rf_model)
